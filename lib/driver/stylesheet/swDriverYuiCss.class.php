2e9320be7a2cbae3088f063ac58347b7e3e5e59d
Thomas <thomas.rabaix@soleoweb.com>
2010-11-22 09:46:11 +0100

    remove tabulation

--- lib/driver/stylesheet/swDriverYuiCss.class.php
+++ lib/driver/stylesheet/swDriverYuiCss.class.php
@@ -11,29 +11,29 @@
  */
 class swDriverYuiCss extends swDriverBase
 {
-	public function doProcessFile($file, $replace = false)
-	{
-		$cmd = sprintf('java -jar %s/swCombinePlugin/lib/vendor/yui/yuicompressor.jar --type css --charset utf-8 %s', sfConfig::get('sf_plugins_dir'), $file);
+  public function doProcessFile($file, $replace = false)
+  {
+    $cmd = sprintf('java -jar %s/swCombinePlugin/lib/vendor/yui/yuicompressor.jar --type css --charset utf-8 %s', sfConfig::get('sf_plugins_dir'), $file);
 
-		$return = false;
-		$output = array();
-		exec($cmd, $output, $return);
+    $return = false;
+    $output = array();
+    exec($cmd, $output, $return);
 
-		if($return != 0)
-		{
-			throw new RuntimeException('unable to compile the asset with yui compressor');
-		}
+    if ($return != 0)
+    {
+      throw new RuntimeException('unable to compile the asset with yui compressor');
+    }
 
-		$optimizedContent = implode("\n", $output);
+    $optimizedContent = implode("\n", $output);
 
-		if ($replace)
-		{
-			return parent::replaceFile($file, $optimizedContent);
-		}
-		else
-		{
-			return $optimizedContent;
-		}
-	}
+    if ($replace)
+    {
+      return parent::replaceFile($file, $optimizedContent);
+    }
+    else
+    {
+      return $optimizedContent;
+    }
+  }
 }
 
